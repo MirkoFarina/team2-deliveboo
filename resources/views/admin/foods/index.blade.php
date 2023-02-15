20ba5f3d2625cc22397109fa9d27ebb94044b229
MirkoFarina <MirkoFarina@outlook.com>
2023-02-15 16:59:56 +0100

    [add] index show e delete

---
+++ resources/views/admin/foods/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container py-5">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Disponibile</th>
+                <th scope="col">Azioni</th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach ($foods as $food )
+                    <tr>
+                    <td>{{ $food->name}} </td>
+                    <td> {{$food->price}} &euro;</td>
+                    <td>
+                        @if ($food->is_available)
+                            Disponibile
+                        @else
+                            Non Disponibile
+                        @endif
+                    </td>
+                    <td>
+                        <a class="btn btn-primary" href=" {{ route('admin.food.show', $food) }} ">
+                            <i class="fa-regular fa-eye"></i>
+                        </a>
+                        <a class="btn btn-warning" href=" {{route('admin.food.edit', $food) }} ">
+                            <i class="fa-solid fa-pencil"></i>
+                        </a>
+                        @include('admin.partials.form-delete', [
+                            'title'   => $food->name,
+                            'route'   => 'admin.food.destroy',
+                            'element' => $food
+                        ])
+                    </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
