21d5d32f60a67ccf9308cbcabbc826b54cff4ef3
DenisConsta <denisconstantin338@gmail.com>
2023-02-15 10:01:44 +0100

    fix user's password length

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('phone_number', 10);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 32);
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
