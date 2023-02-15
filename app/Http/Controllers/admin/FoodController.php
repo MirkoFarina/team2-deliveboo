56678768d49455a4840a73a54fde2456c117282a
MirkoFarina <MirkoFarina@outlook.com>
2023-02-15 17:16:38 +0100

    [add] edit

---
+++ app/Http/Controllers/admin/FoodController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\FoodRequest;
+use App\Models\Food;
+use Illuminate\Http\Request;
+
+class FoodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $foods = Food::all();
+
+        return view('admin.foods.index', compact('foods'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  Food $food
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Food $food)
+    {
+        return view('admin.foods.show', compact('food'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Food $food)
+    {
+        return view('admin.foods.edit', compact('food'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(FoodRequest $request, Food $food)
+    {
+        $form_data = $request->all();
+        dump($form_data);
+        //return redirect()->route('admin.food.edit')->with('edit', "$food->name è stato modificato corretamente");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  Food $food
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Food $food)
+    {
+        $food->delete();
+        return redirect()->route('admin.food.index')->with('delete', 'Il tuo piatto è stato eliminato correttamente');
+    }
+}
---
+++ resources/views/admin/foods/edit.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.admin')
+
+
+@section('content')
+    <div class="container py-5">
+        <form action="{{route('admin.food.update', $food)}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="mb-3">
+                <label for="name" class="form-label">Nome</label>
+                <input type="text" value="{{ old('name', $food->name) }}" class="form-control" id="name" name="name" placeholder="Inserisci il nome ...">
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Prezzo</label>
+                <input type="number" value="{{ old('price', $food->price) }}" class="form-control" id="price" name="price" placeholder="Inserisci il prezzo ...">
+            </div>
+            <div class="mb-3">
+                <label for="ingredients" class="form-label">Ingredienti</label>
+                <input type="text" value="{{ old('ingredients', $food->ingredients) }}" class="form-control" id="ingredients" name="ingredients" placeholder="Inserisci gli ingredienti ...">
+            </div>
+            {{-- <div class="mb-3">
+                <label class="form-label" for="is_available">Disponibilità:</label>
+                <select class="form-select" name="is_available">
+                    <option
+                    @if($food->is_available) selected @endif value="true">Disponibile</option>
+                    <option
+                    @if(!$food->is_available) selected @endif value="false">Non Disponibile</option>
+                </select>
+            </div> --}}
+            {{-- <div class="mb-3">
+                <label for="cover_image" class="form-label @error('cover_image') text-danger @enderror">UPLOAD image
+                </label>
+                <input onchange="showImg(event)" type="file"
+                    class="form-control mb-2 @error('cover_image') is-invalid @enderror" id="cover_image" name="cover_image"
+                    placeholder="IMAGE ... ">
+                @error('cover_image')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="show-img">
+                    <img id="image_thumb_up" src="" alt="">
+                </div>
+            </div> --}}
+            <div class="mb-3 text-center">
+                <button class="btn btn-success" type="submit"> INVIA </button>
+            </div>
+        </form>
+    </div>
+
+
+
+
+    <script>
+        function showImg(event) {
+            const tagImage = document.getElementById('image_thumb_up');
+            tagImage.src = URL.createObjectURL(event.target.files[0]);
+        }
+    </script>
+@endsection
