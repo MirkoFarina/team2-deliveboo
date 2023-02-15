fc332faa9fd9fc72e0a979755dc245c9f0e0ef36
MirkoFarina <MirkoFarina@outlook.com>
2023-02-15 15:56:53 +0100

    [add] font awesome

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,7 +10,8 @@
 
     <title>DASHBOARD</title>
 
-
+    <!-- FONT AWESOME -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css' integrity='sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==' crossorigin='anonymous'/>
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
