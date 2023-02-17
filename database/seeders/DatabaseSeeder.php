3d0313a253e545b76b9e5b8f78bf60214c582906
MirkoFarina <MirkoFarina@outlook.com>
2023-02-17 16:07:05 +0100

    [add] seeder of restaurant category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             CategoryTableSeeder::class,
             OrderTableSeeder::class,
             OrderFoodTableSeeder::class,
+            CategoryRestaurantSeeder::class
         ]);
     }
 }
