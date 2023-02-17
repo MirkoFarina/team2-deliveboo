80ee9a194066273ae93e257b5b743a99f2f70ae9
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-02-17 15:45:22 -0500

    change ui of restaurands cards

--- resources/js/components/partials/ListRestaurants.vue
+++ resources/js/components/partials/ListRestaurants.vue
@@ -11,21 +11,20 @@ export default {
 </script>
 
 <template>
-  <div class="container">
+  <div class="container w-75 my-5">
+    <h2 class="text-center text-uppercase py-5">I nostri ristoranti</h2>
     <div class="row">
         <!-- PER LUCA TUTTO DA STILIZZARE, FARE ANCHE PAGINA PER MENU RISTORANTI SU CUI POI FAREMO ANCHE GLI ORDINI  -->
-
-
-        <div class="col" v-for="restaurant in store.restaurants" :key="restaurant.slug">
-            <div class="card" style="width: 18rem;">
+        <div class="col my-5" v-for="restaurant in store.restaurants" :key="restaurant.slug">
+            <div class="card card-lf m-auto">
             <!-- <img src="..." class="card-img-top" alt="..."> -->
-            <div class="card-body">
-                <h5 class="card-title">{{ restaurant.name_of_restaurant }}</h5>
-                <span v-for="category in restaurant.categories" :key="category.slug" class="badge text-bg-danger">{{category.name}}</span>
-                <p class="card-text">Indirizzo: {{ restaurant.address }}</p>
-                <p class="card-text">Numero: {{ restaurant.phone_number }}</p>
+            <div class="card-body card-body-lf">
+                <h5 class="card-title fs-3 py-3 text-uppercase">{{ restaurant.name_of_restaurant }}</h5>
+                <span v-for="category in restaurant.categories" :key="category.slug" class="badge text-bg-dark mx-1 mb-5">{{category.name}}</span>
+                <p class="card-text fs-5">Indirizzo: {{ restaurant.address }}</p>
+                <p class="card-text fs-5">Numero: {{ restaurant.phone_number }}</p>
                 <p class="card-text">Piatti Disponibili: {{ restaurant.foods.length }}</p>
-                <a href="#" class="btn btn-primary">VAI AL RISTORANTE</a>
+                <a href="#" class="btn btn-outline-secondary mt-5 text-uppercase">Vai al ristorante</a>
             </div>
             </div>
         </div>
@@ -35,5 +34,16 @@ export default {
 
 
 <style lang="scss" scoped>
+    .card-lf {
+        width: 30rem;
+        height: 450px;
+        border: none;
+    }
 
+    .card-body-lf {
+        background-color: #f6f6f6;
+        border-radius: 10px;
+        box-shadow: 2px 3px 2px 1px rgba($color: #000000, $alpha: 0.2);
+        padding: 15px 26px;
+    }
 </style>
