b5d8e994bddaf92706a91ed02d2976598b3e2aaa
MirkoFarina <MirkoFarina@outlook.com>
2023-02-18 18:27:49 +0100

    [add] shopping cart

--- resources/js/data/store.js
+++ resources/js/data/store.js
@@ -3,13 +3,18 @@ import { reactive } from 'vue';
 export const store = reactive({
     restaurants     : [],
     categories      : [],
-    shopping_cart   : [
-        {
-            name:   'pita',
-            price: '20',
-            quantity: 1
-        }
-    ]
+    is_canvas       : false,
+    shopping_cart   : {
+        total_amount : 20,
+        restaurant: 10,
+        foods: [
+            {
+                name: 'pita',
+                price: 10,
+                quantity: 1
+            }
+        ]
+    }
 })
 
 
