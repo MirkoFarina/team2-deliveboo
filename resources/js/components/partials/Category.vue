4d022cb69210fb271a956011d585729e16041fcf
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-02-20 09:58:50 -0500

    work on categories

---
+++ resources/js/components/partials/Category.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+name: 'Category'
+}
+</script>
+
+<template>
+<div class="box">
+        <img :src="category.cover_image" :alt="category.slug">
+        <p>{{ category.name }}</p>
+        <div class="form-check m-2">
+        <input class="form-check-input p-3 border-info" type="checkbox" value="" id="flexCheckIndeterminate">
+        <label class="form-check-label" for="flexCheckIndeterminate">
+        </label>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
