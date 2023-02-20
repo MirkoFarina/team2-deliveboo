05236b76bfe7f5eb5d45afa520dd85c3bf9cd1ec
Giorgio Perez <giorgiop62@gmail.com>
2023-02-20 18:00:21 +0100

    slider

--- resources/js/components/partials/Categories.vue
+++ resources/js/components/partials/Categories.vue
@@ -1,23 +1,56 @@
 <script>
 import Category from './Category.vue'
 import { store } from '../../data/store';
+import {Swiper, SwiperSlide} from 'swiper/vue';
+import {Navigation, Pagination} from 'swiper';
+import 'swiper/css/bundle';
+
 // import axios from 'axios';
 // import { BASE_URL } from '../../data/data';
 
 export default {
   name: 'Categories',
+  components: {
+        Swiper,
+        SwiperSlide,
+        Category
+    },
   data(){
     return{
         store,
     }
     },
-    components: {
-        Category
-    }
+    setup () {
+        return {
+            modules:[Navigation, Pagination]
+        }
+    },
 }
 </script>
 
 <template>
+    <swiper
+:modules="modules"
+navigation
+:slidesPerView="'3'"
+:effect="'coverflow'"
+:pagination="{clickable: true }"
+:coverflowEffect="{
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      }">
+
+      <SwiperSlide  v-for="category in store.categories" :key="'category' + category.id">
+        <div>
+            <Category :category="category" />
+
+        </div>
+      </SwiperSlide>
+      </swiper>
+
   <div class="container mt-5 my-5">
       <div class="boxes">
        <a v-for="category in store.categories" :key="'category' + category.id" >
--- resources/js/pages/ChiSiamo.vue
+++ resources/js/pages/ChiSiamo.vue
@@ -1,11 +1,11 @@
 <script>
-import team  from '../data/team'
+import team from "../data/team";
 export default {
   name: "ChiSiamo",
 
   data() {
     return {
-        team
+      team,
     };
   },
 };
@@ -13,32 +13,22 @@ export default {
 
 <template>
   <h1>Il nostro team</h1>
-  <div class="container d-flex w-80 flex-wrap">
-    <div class="card" style="width: 18rem" v-for="(profile, index) in team" :key="index">
-      <img class="card-img" :src="profile.img" alt="Card image " />
-      <div class="card-body">
-        <h5 class="card-title">{{ profile.name }}</h5>
-        <h5 class="card-title">{{ profile.usrname }}</h5>
-        <h4>{{ profile.rule }}</h4>
-      </div>
-    </div>
+  <div class="card" style="width: 18rem;" v-for="(profile, index) in team" :key="index">
+  <img :src=profile.img class="card-img-top" alt="profile.name">
+  <div class="card-body">
+    <h5 class="card-title">{{ profile.name }}</h5>
+    <h5 class="card-title">{{ profile.usrname }}</h5>
+    <p class="card-text">{{ profile.rule }}</p>
+
+
   </div>
+</div>
+
 </template>
 
 <style lang="scss" scoped>
-h1{
+h1 {
   text-align: center;
-  margin-bottom: 40px;
+  text-transform: uppercase;
 }
-.card-img{
-  object-fit: cover;
-  height: 100%;
-  width: 100%;
-
-}
-.card{
-  margin: 10px;
-}
-
-
 </style>
