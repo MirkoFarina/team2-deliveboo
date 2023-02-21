d3c99828fbf8fc08c5a200e326760018661bc1cc
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-02-21 11:39:08 -0500

    fix ui listrestaurants cards and create component

---
+++ resources/js/components/partials/Card.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+    name: 'Card',
+    props: {
+        restaurant: Object
+    }
+}
+ </script>
+
+<template>
+    <div class="card card-lf">
+        <div class="card-body card-body-lf">
+            <div class="box">
+                <div class="logo">
+                    <img :src="restaurant.cover_image" class="card-img-top" :alt="restaurant.name_of_restaurant">
+                </div>
+                <div class="info">
+                    <h5 class="card-title fs-4 mb-4 text-uppercase">{{ restaurant.name_of_restaurant }}</h5>
+                    <span v-for="category in restaurant.categories" :key="category.slug" class="badge text-bg-dark mx-1 my-1">{{ category.name }}</span>
+                    <p class="card-text fs-6 m-0 mt-4">Indirizzo: {{ restaurant.address }}</p>
+                    <p class="card-text fs-6">Numero: {{ restaurant.phone_number }}</p>
+                </div>
+            </div>
+            <!-- <p class="card-text">Piatti Disponibili: {{ restaurant.foods.length }}</p> -->
+                <router-link :to="{ name: 'detail', params: { slug: restaurant.slug } }" class="btn btn-outline-secondary mt-3 text-uppercase">Vai al ristorante</router-link>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+    .card-lf {
+        width: 600px;
+        height: 300px;
+        border: none;
+        background-color: #f6f6f6;
+        padding: 10px;
+        box-shadow: 1px 2px 2px rgba($color: #000000, $alpha: 0.1);
+        padding: 10px 15px;
+    }
+
+    .card-body-lf {
+        .box {
+            display: flex;
+            justify-content: space-around;
+            .logo {
+                width: 30%;
+                  img {
+                    object-fit: contain;
+                    height: 80%;
+                    width: 80%;
+                 }
+            }
+            .info {
+                width: 60%;
+                height: 200px;
+            }
+        }
+    }
+</style>
--- resources/js/components/partials/ListRestaurants.vue
+++ resources/js/components/partials/ListRestaurants.vue
@@ -1,7 +1,9 @@
 <script>
-import {store} from '../../data/store'
+import { store } from '../../data/store'
+import Card from './Card.vue';
 export default {
     name: 'ListRestaurants',
+    components: {Card},
     data() {
         return {
         store
@@ -14,17 +16,7 @@ export default {
     <div class="container my-5">
         <div v-if="store.filtered_rest.length" class="row">
             <div class="col my-3" v-for="restaurant in store.filtered_rest" :key="restaurant.slug">
-                <div class="card card-lf m-auto">
-                        <img :src="restaurant.cover_image" class="card-img-top" :alt="restaurant.name_of_restaurant">
-                    <div class="card-body card-body-lf">
-                        <h5 class="card-title fs-3 py-3 text-uppercase">{{ restaurant.name_of_restaurant }}</h5>
-                        <span v-for="category in restaurant.categories" :key="category.slug" class="badge text-bg-dark mx-1 mb-1">{{category.name}}</span>
-                        <p class="card-text fs-5">Indirizzo: {{ restaurant.address }}</p>
-                        <p class="card-text fs-5">Numero: {{ restaurant.phone_number }}</p>
-                        <!-- <p class="card-text">Piatti Disponibili: {{ restaurant.foods.length }}</p> -->
-                            <router-link :to="{ name: 'detail', params: { slug: restaurant.slug } }" class="btn btn-outline-secondary mt-5 text-uppercase">Vai al ristorante</router-link>
-                    </div>
-                </div>
+                <Card :restaurant="restaurant"/>
             </div>
         </div>
 
@@ -34,17 +26,7 @@ export default {
 
         <div v-else class="row">
             <div class="col my-3" v-for="restaurant in store.restaurants" :key="restaurant.slug">
-                <div class="card card-lf m-auto">
-                    <div class="card-body card-body-lf">
-                        <img :src="restaurant.cover_image" class="card-img-top" :alt="restaurant.name_of_restaurant">
-                        <h5 class="card-title fs-3 py-3 text-uppercase">{{ restaurant.name_of_restaurant }}</h5>
-                        <span v-for="category in restaurant.categories" :key="category.slug" class="badge text-bg-dark mx-1 mb-1">{{category.name}}</span>
-                        <p class="card-text fs-5">Indirizzo: {{ restaurant.address }}</p>
-                        <p class="card-text fs-5">Numero: {{ restaurant.phone_number }}</p>
-                        <!-- <p class="card-text">Piatti Disponibili: {{ restaurant.foods.length }}</p> -->
-                            <router-link :to="{ name: 'detail', params: { slug: restaurant.slug } }" class="btn btn-outline-secondary mt-5 text-uppercase">Vai al ristorante</router-link>
-                    </div>
-                </div>
+                <Card :restaurant="restaurant"/>
             </div>
         </div>
 
@@ -53,21 +35,4 @@ export default {
 
 
 <style lang="scss" scoped>
-    .card-lf {
-        width: 25rem;
-        height: 500px;
-        border: none;
-        img {
-            object-fit: cover;
-            height: 50%;
-            width: 50%;
-        }
-    }
-
-    .card-body-lf {
-        background-color: #f6f6f6;
-        border-radius: 10px;
-        box-shadow: 1px 2px 2px rgba($color: #000000, $alpha: 0.1);
-        padding: 15px 26px;
-    }
 </style>
