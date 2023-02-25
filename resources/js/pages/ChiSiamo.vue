cda479b9a7130148dedb44a411ed71a9c212f9c4
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-02-25 12:04:29 -0500

    fix chisiamo page

--- resources/js/pages/ChiSiamo.vue
+++ resources/js/pages/ChiSiamo.vue
@@ -11,35 +11,31 @@ export default {
 };
 </script>
 
+
+
 <template>
-  <h1>Il nostro team</h1>
-  <div class="container">
-  <div class="card"  style="width: 18rem;" v-for="(profile, index) in team" :key="index">
-  <img :src="profile.img" class="card-img-top" alt="profile.name">
-  <div class="card-body">
-    <h5 class="card-title">{{ profile.name }}</h5>
-    <h5 class="card-title">{{ profile.usrname }}</h5>
-    <p class="card-text">{{ profile.rule }}</p>
-
-
+  <h1 class="text-center text-uppercase py-2 fs-4">Il nostro team</h1>
+  <div class="container w-75">
+    <div class="row">
+            <div class="col-4 card mx-5 my-5" v-for="(profile, index) in team" :key="index" style="width: 15rem;">
+                <img :src="profile.img" class="card-img-top p-2" :alt="profile.name">
+                <div class="card-body p-4">
+                    <h5 class="card-title">{{ profile.name }}</h5>
+                    <h5 class="card-title">{{ profile.usrname }}</h5>
+                    <p class="card-text">{{ profile.rule }}</p>
+                </div>
+             </div>
+    </div>
   </div>
-</div>
-</div>
+
+
+
+
 
 </template>
 
 <style lang="scss" scoped>
-h1 {
-  text-align: center;
-  text-transform: uppercase;
-}
-.container {
-  display: flex;
-}
-.card{
-    margin:20px;
-    border-radius:20px;
-}
+
 img{
     width:100%;
     height:100%;
