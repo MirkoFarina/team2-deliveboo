bf6ccbd66483ca107a2b6a8cbbbe012fb5c22cc3
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-02-25 10:30:38 -0500

    fix jumbo ui

--- resources/js/components/partials/Jumbo.vue
+++ resources/js/components/partials/Jumbo.vue
@@ -7,16 +7,16 @@ export default {
 <template>
     <div class="jumbo-container w-100 d-flex align-items-center flex-column justify-content-end mb-5">
     </div>
-        <div class="wave down">
+        <!-- <div class="wave down">
                 <svg class="wave-1hkxOo" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100" preserveAspectRatio="none"><path class="wavePath-haxJK1 animationPaused-2hZ4IO" d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z" fill="currentColor"></path></svg>
-              </div>
+              </div> -->
 </template>
 
 
 
 <style lang="scss" scoped>
 .wave svg {
-  color: #FF5758;
+  color: #D85355;
   display: block;
   margin-bottom: 100px;
 }
@@ -25,7 +25,7 @@ export default {
   margin-top: -48px;
 }
     .jumbo-container{
-        height: 580px;
+        height: 500px;
         background-position: center;
         background-size: cover;
         background-image: url("../../assets/img/jumbotron.jpg");
@@ -54,7 +54,10 @@ export default {
     }
     @media  screen and (max-width:390px) {
        .jumbo-container{
-        height: 290px;
+        height: 300px;
+        background-position: center;
+        background-size:contain;
+        background-repeat: no-repeat;
        }
     }
 
