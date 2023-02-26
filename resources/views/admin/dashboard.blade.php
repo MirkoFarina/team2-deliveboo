655311587af7b99e7768f16926a4a02c13d56ba5
MirkoFarina <MirkoFarina@outlook.com>
2023-02-26 21:14:50 +0100

    [add] text in welcome page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 <div class="container">
-    <h1 class="text-center text-white">
-        BENVENUTO NELLA TUA DASHBOARD
+    <h1 class="text-center text-white my-5">
+        BENVENUTO NELLA TUA DASHBOARD <br>
+        TI AUGURIAMO UNA BUONA PERMANENZA
     </h1>
 </div>
 @endsection
