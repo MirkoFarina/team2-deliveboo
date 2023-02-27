ddb9036788cb30d0b4840f253cc8ff708b74c899
Giorgio Perez <giorgiop62@gmail.com>
2023-02-27 16:11:44 +0100

    transition

--- resources/js/pages/ChiSiamo.vue
+++ resources/js/pages/ChiSiamo.vue
@@ -17,11 +17,11 @@ export default {
   <h1 class="text-center text-uppercase pt-5 fs-4">The team</h1>
   <div class="container d-flex align-items-center w-100 justify-content-center">
     <div class="row">
-            <div class="col-4 card mx-1 my-5" v-for="(profile, index) in team" :key="index" style="width: 15rem;">
+            <div class="col-4 card mx-1 my-5 profile" v-for="(profile, index) in team" :key="index" style="width: 15rem;">
                 <img :src="profile.img" class="card-img-top p-2" :alt="profile.name">
                 <div class="card-body p-4">
-                    <h5 class="card-title">{{ profile.name }}</h5>
-                    <h5 class="card-title">{{ profile.usrname }}</h5>
+                    <h5 class="card-title fw-bold">{{ profile.name }}</h5>
+                    <h5 class="card-title fw-bold">{{ profile.usrname }}</h5>
                     <p class="card-text">{{ profile.rule }}</p>
                 </div>
              </div>
@@ -41,6 +41,15 @@ img{
     height:100%;
     object-fit:cover;
 }
+.profile{
+    background-color:#43ddbe;
+    transition:transform .2s;
+    cursor: pointer;
+    &:hover{
+         transform: scale(0.9);
+    }
+}
+
 
 
 </style>
