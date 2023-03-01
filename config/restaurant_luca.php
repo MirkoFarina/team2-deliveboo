9c438b2af9d5baa4f420ce636e60d064cb085e90
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-01 09:26:04 -0500

    add array images foods

---
+++ config/restaurant_luca.php
@@ -0,0 +1,311 @@
+<?php
+return [
+    [
+        "id" => "1",
+        "email"=>"info@sorbillo.it",
+        "address"=> "Via dei Tribunali 32",
+        "name_of_restaurant"=>"Sorbillo",
+        "phone_number"=> "081446646",
+        "website"=> "https://www.sorbillo.it/",
+        "cover_image"=> "https://i.ytimg.com/vi/liw8QGnaPbE/maxresdefault.jpg",
+        "foods" =>[
+            [
+                "name"=>"Margherita",
+                "price" => 5.00,
+                "is_available" => true,
+                "ingredient"  => "Mozzarella, Pomodoro",
+                "cover_image" => "https://cdn.shopify.com/s/files/1/0586/6795/8427/articles/Margherita-9920.jpg?crop=center&height=915&v=1644590028&width=1200"
+            ],
+            [
+                "name"=>"Marinara",
+                "price" =>4.50,
+                "is_available" =>true,
+                "ingredient"  => "Pomodoro, Acciughe",
+                "cover_image" => "https://primochef.it/wp-content/uploads/2018/05/SH_pizza_alla_marinara.jpg"
+            ],
+            [
+                "name"=>"Salsiccia e patate",
+                "price" =>8.00,
+                "is_available" =>false,
+                "ingredient"  => "Mozzarella, Patate, Salsiccia",
+                "cover_image" => "https://i0.wp.com/www.ibocconighiottidilolly.it/wp-content/uploads/2021/04/IMG_6379-compressed-scaled.jpg?resize=750%2C500&ssl=1"
+            ],
+            [
+                "name"=>"Diavola",
+                "price" =>5.50,
+                "is_available" =>true,
+                "ingredient"  => "Mozzarella, Pomodoro, Salame Piccante",
+                "cover_image" => "https://www.ristorantepizzeriajanna.com/wp-content/uploads/2017/09/pizza_diavola.jpg"
+            ],
+            [
+                "name" => "Margherita Bufala D.O.P.",
+                "price" => 7.50,
+                "is_available" => true,
+                "ingredient"  => "Mozzarella di Bufala D.O.P., Pomodoro, Parmigiano Reggiano, Olio Extra Vergine di Oliva Biologico",
+                "cover_image" => "https://magazine.olivyou.com/wp-content/uploads/2018/10/gino-margherita-ridimensionato-e1539588511969.jpg"
+            ],
+            [
+                "name" => "Fritto Antipasto",
+                "price" => 13,
+                "is_available" => true,
+                "ingredient"  => "Gamberetti, Acciughe, Triglie, Merluzzetti, Calamari, Semola, Sale fino",
+                "cover_image" => "https://www.giallozafferano.it/images/236-23670/Fritto-misto-di-pesce_450x300.jpg"
+            ],
+            [
+                "name" => "Prosciutto e Mozzarella",
+                "price" => 12,
+                "is_available" => true,
+                "ingredient"  => "Crudo di Parma Dop e Mozzarella di Bufala Campana Dop",
+                "cover_image" => "https://www.veroni.it/wp-content/uploads/2021/06/Veroni_mozzarella-e-crudo_blog-1080x675.jpg"
+            ],
+            [
+                "name" => "Polpo con Patate all'Insalata",
+                "price" => 12,
+                "is_available" => true,
+                "ingredient"  => "Condito con una Citronette al Prezzemolo",
+                "cover_image" => "https://statics.cucchiaio.it/content/cucchiaio/it/ricette/2009/12/ricetta-insalata-di-polpo-con-patate/jcr:content/header-par/image_single.img10.jpg/1593161805863.jpg"
+            ],
+            [
+                "name" => "Scarpariello",
+                "price" => 12,
+                "is_available" => true,
+                "ingredient"  => "La ricetta dei Quartieri: Ziti con Pomodorini Gialli e Rossi, Pecorino Romano, Parmigiano Reggiano e Peperoncino",
+                "cover_image" => "https://cucina.ottopagine.it/public/arc/foto/scarpariello.jpg"
+            ],
+            [
+                "name" => "Polpette al Ragù",
+                "price" => 11,
+                "is_available" => true,
+                "ingredient"  => "Carne di Manzo, Pane e Parmigiano Reggiano Dop affogata nel Sugo di Pomodoro",
+                "cover_image" => "https://blog.giallozafferano.it/allacciateilgrembiule/wp-content/uploads/2021/10/polpette-alla-napoletana2-720x480.jpg"
+            ],
+        ]
+    ],
+    [
+            "id" => "2",
+            "email"=> "info@leggimenu.it",
+            "address"=> "Vico S.Giuseppe Cristofaro 4",
+            "name_of_restaurant"=>"Pastena",
+            "phone_number"=> "08119138270",
+            "website"=> "https://www.leggimenu.it/menu/pastena",
+            "cover_image"=> "https://lh3.googleusercontent.com/p/AF1QipPSaZjgeLG12eE90gM9pAlP9DCuFmniOqNF65PL=w1080-h608-p-no-v0",
+            "foods" =>[
+                [
+                    "name" => "FrittaCrock",
+                    "price" => 3.50,
+                    "is_available" => true,
+                    "ingredient"  => "Tonnarelli, Ragù alla bolognese, Piselli di stagione, Besciamella, Pannatura croccante",
+                    "cover_image" => "https://media-assets.lacucinaitaliana.it/photos/620fbf55eada9bef3c3958bd/1:1/w_2560%2Cc_limit/paccheri.jpg"
+                ],
+                [
+                    "name"=>"Carbonara",
+                    "price" =>5.00,
+                    "is_available" =>true,
+                    "ingredient"  => "Uovo, Pecorino, Rigatoni",
+                    "cover_image" => "https://cdn.shortpixel.ai/spai/q_lossy+ret_img+to_auto/https://www.ildenaro.it/wp-content/uploads/2021/11/Carbonara-Foto-dalla-pagina-di-PasteNa-882x1024.jpeg"
+                ],
+                [
+                    "name"=>"Genovese",
+                    "price" =>8.5,
+                    "is_available" =>true,
+                    "ingredient"  => "Cipolle, Spaghetti",
+                    "cover_image" => "https://www.giallozafferano.it/images/180-18019/Genovese_650x433_wm.jpg"
+                ],
+                [
+                    "name"=>"Nerano",
+                    "price" =>7,
+                    "is_available" =>true,
+                    "ingredient"  => "Pomodoro",
+                    "cover_image" => "https://www.giallozafferano.it/images/ricette/169/16964/foto_hd/hd650x433_wm.jpg"
+                ],
+                [
+                    "name"=>"Bolognese",
+                    "price" =>5.5,
+                    "is_available" =>false,
+                    "ingredient"  => "Carne",
+                    "cover_image" => "https://www.misya.info/wp-content/uploads/2017/02/tagliatelle-alla-bolognese.jpg"
+                ],
+                [
+                    "name" => "Cacio e Cozze",
+                    "price" => 9,
+                    "is_available" => true,
+                    "ingredient"  => "Tonnarelli, Pecorino D.O.P., Cozza Nostrana, Lattuga di Mare, Polvere di Cozze",
+                    "cover_image" => "https://blog.giallozafferano.it/cuochinprogress/wp-content/uploads/2020/06/spaghetti-cacio-e-pepe-con-cozze-def1.jpg"
+                 ],
+                [
+                    "name" => "Spaghetto all'Italiana",
+                    "price" => 6,
+                    "is_available" => true,
+                    "ingredient"  => "Spaghetti fatti in casa, passata di pomodoro san marzano",
+                    "cover_image" => "https://media-cdn.tripadvisor.com/media/photo-s/0d/63/fd/cc/spaghetti-all-italiana.jpg"
+                ],
+                [
+                    "name" => "Ravioli alla Genovese",
+                    "price" => 6,
+                    "is_available" => true,
+                    "ingredient"  => "Sfoglia di Pasta alla Carota, Stufato di Manzo e Cipolle, Salsa di Pecorino",
+                    "cover_image" => "https://www.salepepe.it/files/2014/11/ravioli-genovesi4-e1539952596239-1140x636.jpg"
+                ],
+                [
+                    "name" => "Ravioli d'Autunno",
+                    "price" => 6,
+                    "is_available" => true,
+                    "ingredient"  => "Sfoglia di pasta bianca, crema di patate, misticanza di funghi, sentore di gorgonzola",
+                    "cover_image" => "https://www.ideegreen.it/wp-content/uploads/2014/05/come-fare-i-ravioli.jpg"
+                ],
+                [
+                    "name" => "Polpette al Sugo",
+                    "price" => 6,
+                    "is_available" => true,
+                    "ingredient"  => "Macinato di Scottona, Pane, Pepe, Prezzemolo, Ristretto di Pomodoro",
+                    "cover_image" => "https://www.ideegreen.it/wp-content/uploads/2014/05/come-fare-i-ravioli.jpg"
+                ],
+            ],
+        ],
+
+            [
+                "id" => "3",
+                "email"=>"cicciottoristorante@gmail.com",
+                "address"=> "Calata Ponticello a Marechiaro 32",
+                "name_of_restaurant"=>"Cicciotto",
+                "phone_number"=> "081 575 1165",
+                "website"=> "https://www.trattoriadacicciotto.it/",
+                "cover_image"=> "https://www.trattoriadacicciotto.it/wp-content/uploads/2021/10/hero-home-trattoria-da-cicciotto-a-marechiaro-dal-1942.jpg",
+                "foods" =>[
+                    [
+                        "name" => "Insalata Caprese",
+                        "price" => 10,
+                        "is_available" => true,
+                        "ingredient"  => "Mozzarella di Bufala, Pomodori fiascone, Olio extravergine di oliva, origano",
+                        "cover_image" => "https://www.tavolartegusto.it/wp/wp-content/uploads/2017/07/Insalata-Caprese-Ricetta-originale-Insalata-Caprese-pomodori-e-mozzarella-.jpg"
+                    ],
+                    [
+                        "name" => "Insalata di Mare",
+                        "price" => 15,
+                        "is_available" => true,
+                        "ingredient"  => "Cozze, vongole, polpo, calamari, surimi",
+                        "cover_image" => "https://blog.giallozafferano.it/valeriaciccotti/wp-content/uploads/2020/11/INSALATA-DI-MARE-12.jpg"
+                    ],
+                    [
+                        "name" => "Impepata di Cozze",
+                        "price" => 15,
+                        "is_available" => true,
+                        "ingredient"  => "Cozze, Pepe nero, Prezzemolo, Limone",
+                        "cover_image" => "https://www.giallozafferano.it/images/229-22976/Impepata-di-cozze_650x433_wm.jpg"
+                    ],
+                    [
+                        "name"=>"Spaghetti alle vongole",
+                        "price" =>20,
+                        "is_available" =>true,
+                        "ingredient"  => "Spaghetti, Vongole, Aglio, Limone",
+                        "cover_image" => "https://blog.giallozafferano.it/nocemoscata/wp-content/uploads/2020/07/MG_1933.jpg"
+                    ],
+                    [
+                        "name"=>"Risotto alla pescatora",
+                        "price" =>18,
+                        "is_available" =>true,
+                        "ingredient"  => "Risotto, Calamari, Vongole",
+                        "cover_image" => "https://blog.giallozafferano.it/cuochinprogress/wp-content/uploads/2018/11/risotto-alla-pescatora1-720x600.jpg"
+                    ],
+                    [
+                        "name"=>"Spaghetti con riccio",
+                        "price" =>25,
+                        "is_available" =>false,
+                        "ingredient"  => "Spaghetti, Riccio",
+                        "cover_image" => "https://www.giallozafferano.it/images/185-18529/Pasta-con-ricci-e-pomodori_650x433_wm.jpg"
+                    ],
+                    [
+                        "name"=>"Genovese di mare",
+                        "price" =>20,
+                        "is_available" =>true,
+                        "ingredient"  => "Cipolle, Tonno, Spaghetti",
+                        "cover_image" => "https://staticfanpage.akamaized.net/wp-content/uploads/sites/21/2022/07/Genovese-di-mare-1200x675.jpg"
+                    ],
+                    [
+                        "name"=>"Pesce spada",
+                        "price" =>15,
+                        "is_available" =>true,
+                        "ingredient"  => "Pesce Spada",
+                        "cover_image" => "https://primochef.it/wp-content/uploads/2019/04/SH_pesce_spada_grigliato-1200x800.jpg"
+                    ],
+                    [
+                        "name"=>"Frittura di mare",
+                        "price" =>15,
+                        "is_available" =>true,
+                        "ingredient"  => "Calamari, Pastella",
+                        "cover_image" => "https://www.giallozafferano.it/images/ricette/195/19593/foto_hd/hd650x433_wm.jpg"
+                    ],
+                    [
+                        "name" => "Filetto di Manzo con Tagliata di Patate",
+                        "price" => 25,
+                        "is_available" => true,
+                        "ingredient"  => "Controfiletto di manzo, sale grosso, olio di oliva, patate, rosmarino",
+                        "cover_image" => "https://blog.giallozafferano.it/studentiaifornelli/wp-content/uploads/2020/11/ricetta-tagliata-di-manzo-con-patate-arrosto-ricetta-facile-secondo.jpg"
+                    ],
+                ],
+
+            ],
+            [
+                "id" => "4",
+                "email"=>"franchising@otalebanokebab.it",
+                "address"=> "Via Porta di Massa 8",
+                "name_of_restaurant"=>"O'Talebano",
+                "phone_number"=> "0818842253",
+                "website"=> "http://www.otalebano.it/",
+                "cover_image"=> "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBIo583BCc1_ZPIlh_dwGL9MjMn9u4_K4nO7qIit-Fu1I9KMwfwa67QKAIhN-KKaH-PA&usqp=CAU",
+                "foods" =>[
+                    [
+                        "name"=>"Pita Pig",
+                        "price" => 6,
+                        "is_available" =>true,
+                        "ingredient"  => "Kebab di pollo, Mortadella igp, Provola, Pesto",
+                        "cover_image" => "http://www.otalebano.it/images/content/03_chef-team_550x740.jpg"
+                    ],
+                    [
+                        "name"=>"Pita Peppers",
+                        "price" =>4.5,
+                        "is_available" =>true,
+                        "ingredient"  => "Kebab di pollo, peperoncini verdi, Fritti saltati in padella con pomodorini, crema di parmigiano",
+                         "cover_image" => "http://www.otalebano.it/images/content/07_chef-team_550x740.jpg"
+                    ],
+                    [
+                        "name"=>"Pita Classica",
+                        "price" =>4,
+                        "is_available" =>false,
+                        "ingredient"  => "Kebab di pollo, Patatine, Insalata, Pomodori, Cipolle, Salse",
+                        "cover_image" => "http://www.otalebano.it/images/content/01_chef-team_550x740.jpg"
+                    ],
+                    [
+                        "name"=>"Pita Pastrami",
+                        "price" =>5.5,
+                        "is_available" =>true,
+                        "ingredient"  => "Punta di petto di manzo marinato speziato, Affumicato e cotto al vapore, rucola, pomodorini",
+                        "cover_image" => "http://www.otalebano.it/images/content/06_chef-team_550x740.jpg"
+                    ],
+                    [
+                        "name" => "Pita Verdure",
+                        "price" => 5.5,
+                        "is_available" => true,
+                        "ingredient"  => "Kebab di pollo, Limone, Melanzane, Peperoni, Zucchine, Maionese",
+                        "cover_image" => "http://www.otalebano.it/images/content/04_chef-team_550x740.jpg"
+                    ],
+                    [
+                        "name" => "Pita Mikonos",
+                        "price" => 5.5,
+                        "is_available" => true,
+                        "ingredient"  => "Kebab di pollo, Rucola, Aceto Balsamico, Feta, Olive",
+                        "cover_image" => "http://www.otalebano.it/images/content/05_chef-team_550x740.jpg"
+                    ],
+                    [
+                        "name" => "Pita Americana",
+                        "price" => 5.5,
+                        "is_available" => true,
+                        "ingredient"  => "Kebab di pollo, bacon, cheddar, Barbecue, Maionese",
+                        "cover_image" => "http://www.otalebano.it/images/content/02_chef-team_550x740.jpg"
+                    ],
+
+                ],
+
+            ],
+
+        ];
